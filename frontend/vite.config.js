10328d8a6d6a659b0b02e2bd51e6f03828ef70f9
James Jackson <jmjb318@gmail.com>
2026-03-26 16:51:05 -0700

    Fix vite base path for Vercel deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/itp303-submissions-the-jamesjackson/final_project/completed_final/final/dist/',
+  base: '/',
   server: {
     proxy: {
       '/api': 'http://localhost:3000'
